5862fc1f26a1950c23d0af2f8b79c918253a18ff
ImJoyler <filippo.mussi@edu.unife.it>
2026-04-12 18:02:25 +0200

    PearlChanges

--- src/main/kotlin/shizo/utils/skyblock/kuudra/pearlUtils.kt
+++ src/main/kotlin/shizo/utils/skyblock/kuudra/pearlUtils.kt
@@ -8,67 +8,84 @@ import shizo.Shizo.mc
 
 object PearlUtils {
 
-    fun predictPearlLanding(): Vec3? {
-        val player = mc.player ?: return null
+    fun predictPearlLandingFromEntity(entity: net.minecraft.world.entity.Entity): Vec3? {
         val level = mc.level ?: return null
 
-        val STAND_EYE_HEIGHT = 1.6200000047683716
-        val SNEAK_EYE_HEIGHT = 1.5399999618530273
-        val eyeHeight = if (player.isCrouching) SNEAK_EYE_HEIGHT else STAND_EYE_HEIGHT
+        var px = entity.x
+        var py = entity.y
+        var pz = entity.z
 
-        val pitch = player.xRot.toDouble()
-        val yaw = player.yRot.toDouble()
+        var vx = entity.deltaMovement.x
+        var vy = entity.deltaMovement.y
+        var vz = entity.deltaMovement.z
 
-        val pitchRad = pitch / 180.0 * Math.PI
-        val yawRad = yaw / 180.0 * Math.PI
-
-        var px = player.x
-        var py = player.y + eyeHeight
-        var pz = player.z
-
-        px -= Math.cos(yawRad) * 0.1600000023841858
-        py -= 0.10000000149011612
-        pz -= Math.sin(yawRad) * 0.1600000023841858
-
-        val f = 0.4
-
-        var motionX = -Math.sin(yawRad) * Math.cos(pitchRad) * f
-        var motionZ = Math.cos(yawRad) * Math.cos(pitchRad) * f
-        var motionY = -Math.sin(pitchRad) * f
-
-        val f1 = Math.sqrt(motionX * motionX + motionY * motionY + motionZ * motionZ)
-        val velocity = 1.5
-        motionX = (motionX / f1) * velocity
-        motionY = (motionY / f1) * velocity
-        motionZ = (motionZ / f1) * velocity
-
-
-        for (i in 0..300) {
+        repeat(1000) {
             val start = Vec3(px, py, pz)
-            val end = Vec3(px + motionX, py + motionY, pz + motionZ)
+            val end = Vec3(px + vx, py + vy, pz + vz)
 
-            val hitResult = level.clip(
-                ClipContext(
-                    start, end,
-                    ClipContext.Block.COLLIDER,
-                    ClipContext.Fluid.NONE,
-                    player
-                )
+            val hit = level.clip(
+                ClipContext(start, end, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, entity)
             )
-            if (hitResult.type == HitResult.Type.BLOCK) {
-                val hitPos = (hitResult as BlockHitResult).blockPos
-                return Vec3(hitPos.x + 0.5, hitPos.y.toDouble() + 1.0, hitPos.z + 0.5)
+
+            if (hit.type == HitResult.Type.BLOCK) {
+                return (hit as BlockHitResult).location
             }
 
-            px += motionX
-            py += motionY
-            pz += motionZ
+            px += vx
+            py += vy
+            pz += vz
 
-            motionX *= 0.9900000095367432
-            motionY *= 0.9900000095367432
-            motionZ *= 0.9900000095367432
-            motionY -= 0.029999999329447746
+            vx *= 0.99
+            vy *= 0.99
+            vz *= 0.99
+            vy -= 0.03
         }
         return null
     }
-}
+
+    fun predictPearlLandingFromPlayer(): Vec3? {
+        val player = mc.player ?: return null
+        val level = mc.level ?: return null
+
+        val yaw = player.yRot.toDouble()
+        val pitch = player.xRot.toDouble()
+
+        val eyeHeight = if (player.isCrouching) 1.54 else 1.62
+
+        var px = player.x - (Math.cos(Math.toRadians(yaw)) * 0.16)
+        var py = player.y + eyeHeight - 0.1 - 0.1
+        var pz = player.z - (Math.sin(Math.toRadians(yaw)) * 0.16)
+
+        val f = 0.4
+        var vx = -Math.sin(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch)) * f
+        var vy = -Math.sin(Math.toRadians(pitch)) * f
+        var vz = Math.cos(Math.toRadians(yaw)) * Math.cos(Math.toRadians(pitch)) * f
+
+        val mag = Math.sqrt(vx * vx + vy * vy + vz * vz)
+        vx = (vx / mag) * 1.5
+        vy = (vy / mag) * 1.5
+        vz = (vz / mag) * 1.5
+
+        repeat(1000) {
+            val start = Vec3(px, py, pz)
+            val end = Vec3(px + vx, py + vy, pz + vz)
+
+            val hit = level.clip(
+                ClipContext(start, end, ClipContext.Block.COLLIDER, ClipContext.Fluid.NONE, player)
+            )
+
+            if (hit.type == HitResult.Type.BLOCK) {
+                return (hit as BlockHitResult).location
+            }
+
+            px += vx
+            py += vy
+            pz += vz
+
+            vx *= 0.99
+            vy *= 0.99
+            vz *= 0.99
+            vy -= 0.03
+        }
+        return null
+    }}
